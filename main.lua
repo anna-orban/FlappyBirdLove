2186214f2a08e74b470db89329adb526ec09e7fb
Anna Orban <anna.orban@yahoo.com>
2018-09-05 21:51:41 +0100

    Pipe collision

--- main.lua
+++ main.lua
@@ -3,31 +3,41 @@ local g = 500
 local jumpSpeed = 165
 local pipeWidth = 54
 local pipeX 
-local pipeY = 250
-local pipeYmin = 30
-local pipeSpace = 100
+local pipeSpaceY = 250
+local pipeSpaceYmin = 54
+local pipeSpaceHeight = 100
+local birdWidth = 30
+local birdHeight = 25
 
+function resetPipe()
+    pipeSpaceY = love.math.random(pipeSpaceYmin, SCREEN_HEIGHT - pipeSpaceHeight - pipeSpaceYmin)
+    pipeX = SCREEN_WIDTH
+end
 
 function love.load()
     birdY = 200
     birdX = 62
     birdYSpeed = 0
-    pipe2Y = pipeY + pipeSpace
+    bottomPipeY = pipeSpaceY + pipeSpaceHeight
     pipeX = SCREEN_WIDTH - pipeWidth 
-    function resetPipe()
-        pipeY = love.math.random(pipeYmin, SCREEN_HEIGHT - pipeSpace - pipeYmin)
-        pipe2Y = SCREEN_HEIGHT-pipeY + pipeSpace
-        pipeX = SCREEN_WIDTH + pipeWidth
-    end
     resetPipe()
 end
 
 function love.update(dt)
     birdYSpeed = birdYSpeed + (g * dt)
-    birdY = birdY + birdYSpeed * dt
+    birdY = birdY + birdYSpeed * dt 
     pipeX = pipeX - (60 * dt)
     if (pipeX + pipeWidth) < 0 then
         resetPipe()
+    end
+    if birdX < (pipeX + pipeWidth) and 
+       (birdX + birdWidth) > pipeX and
+       (
+          birdY < pipeSpaceY or
+         (birdY + birdHeight) > (pipeSpaceY + pipeSpaceHeight)
+       )
+    then
+        love.load()
     end
 end
 
@@ -35,10 +45,10 @@ function love.draw()
     love.graphics.setColor(.13, .36, .46)
     love.graphics.rectangle('fill', 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
     love.graphics.setColor(.87, .84, .27)
-    love.graphics.rectangle('fill', birdX, birdY, 30, 25)
+    love.graphics.rectangle('fill', birdX, birdY, birdWidth, birdHeight)
     love.graphics.setColor(.37, .82, .28)
-    love.graphics.rectangle('fill', pipeX , 0, pipeWidth, pipeY)
-    love.graphics.rectangle('fill', pipeX, pipe2Y, pipeWidth, SCREEN_HEIGHT-pipe2Y)
+    love.graphics.rectangle('fill', pipeX , 0, pipeWidth, pipeSpaceY)
+    love.graphics.rectangle('fill', pipeX, bottomPipeY, pipeWidth, SCREEN_HEIGHT - bottomPipeY)
 end
 
 function love.keypressed(key)
